89078f95d7aa6f870ea4656a2e516c753e9b48bb
GabyMarapi <gaby1017x@gmail.com>
2018-10-03 18:43:37 -0500

    Updates

---
+++ precache-manifest.040a5b28d0c9e6a8fe6bfb6124132814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50626c473dcd3c18512c",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/runtime~main.50626c47.js"
+  },
+  {
+    "revision": "9868c8889411c4e5c206",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/main.9868c888.chunk.js"
+  },
+  {
+    "revision": "ca9d58540164a0fc42b1",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/1.ca9d5854.chunk.js"
+  },
+  {
+    "revision": "9868c8889411c4e5c206",
+    "url": "/lim-2018-01-todo-list-frameworks/static/css/main.0f5ee41d.chunk.css"
+  },
+  {
+    "revision": "8ba7a2290f030a1d25c1db6f031adb59",
+    "url": "/lim-2018-01-todo-list-frameworks/index.html"
+  }
+];
